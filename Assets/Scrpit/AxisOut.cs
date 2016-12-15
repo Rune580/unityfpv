6ccb2952b49dc71b1b1129ce19d3602327463bb8
RuneMasterGaming <RuneMasterGaming@users.noreply.github.com>
2016-12-14 19:47:41 -0500

    First Upload
    Uploading to git

---
+++ Assets/Scrpit/AxisOut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AxisOut : MonoBehaviour {
+
+	public Text axisVal;
+
+	public string axis = "Vertical";
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+    axisVal.text = Input.GetAxis(axis).ToString();
+  }
+}
---
+++ Assets/Scrpit/SpinText.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpinText : MonoBehaviour {
+
+	public Text spinningText;
+
+	private const int speed = 15;
+	private const int top = 20;
+	private const int bottem = 5;
+	private int delay;
+	private int val;
+	private bool direction;
+
+	// Use this for initialization
+	void Start () {
+		val = 5;
+		spinningText.text = val.ToString();
+		delay = speed;
+		direction = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (delay > 0)
+		{
+			delay = delay - 1;
+		}
+		else
+		{
+			if (direction)
+			{
+				val = val + 1;
+			}
+			else
+			{
+				val = val - 1;
+			}
+			spinningText.text = val.ToString();
+			if (val >= top)
+			{
+				direction = false;
+			}
+			else if (val <= bottem)
+			{
+				direction = true;
+			}
+			delay = speed;
+		}
+	}
+}
